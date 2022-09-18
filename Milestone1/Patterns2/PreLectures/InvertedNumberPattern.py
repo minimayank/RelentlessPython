b9f73bb2a07eb6dce1791c22d91a50570e44641d
Mayank kumar <42250319+minimayank@users.noreply.github.com>
2022-09-18 23:40:05 +0530

    Inverted Number Pattern

---
+++ Milestone1/Patterns2/PreLectures/InvertedNumberPattern.py
@@ -0,0 +1,13 @@
+## Read input as specified in the question
+## Print the required output in given format
+n=int(input());
+i=n;
+while i>0:
+    x=i;
+    j=i;
+    while j>0:
+        print(x,end="");
+        j-=1;
+        
+    print();
+    i-=1;
