95cc8503db05bc300f0ae91a0af96477923c567a
Mayank kumar <42250319+minimayank@users.noreply.github.com>
2022-09-18 23:37:18 +0530

    ReverseNumberPattern

---
+++ Milestone1/Patterns1/PreLectures/ReverseNumberPattern.py
@@ -0,0 +1,12 @@
+## Read input as specified in the question
+## Print the required output in given format
+
+n=int(input());
+i=1;
+while(i<=n):
+    j=i;
+    while j>=1:
+        print(j,end="");
+        j=j-1;
+    print();    
+    i+=1;    
