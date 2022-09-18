2e1d2cd80456948661a4ca57e56212f02ac468dc
Mayank kumar <42250319+minimayank@users.noreply.github.com>
2022-09-18 23:37:51 +0530

    Number pattern 1

---
+++ Milestone1/Patterns1/PreLectures/NumberPattern1.py
@@ -0,0 +1,50 @@
+from os import *
+from sys import *
+from collections import *
+from math import *
+
+from os import *
+from sys import *
+from collections import *
+from math import *
+
+from os import *
+from sys import *
+from collections import *
+from math import *
+
+from os import *
+from sys import *
+from collections import *
+from math import *
+
+from os import *
+from sys import *
+from collections import *
+from math import *
+
+## Read input as specified in the question.
+## Print output as specified in the question.
+
+n=int(input());
+i=1;
+while i<=n:
+    j=i;
+    while j>0:
+        print(1,end="");
+        j-=1;
+    print();
+    i+=1;
+
+
+
+
+
+
+
+
+
+
+
+
+
