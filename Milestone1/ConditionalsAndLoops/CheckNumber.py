ea9198ad8daf334912f2bd0b6796bc80cb7a113a
Mayank kumar <42250319+minimayank@users.noreply.github.com>
2022-09-18 23:29:44 +0530

    CheckNumber

---
+++ Milestone1/ConditionalsAndLoops/CheckNumber.py
@@ -0,0 +1,10 @@
+# Read input as sepcified in the question
+# Print output as specified in the question
+
+n=int(input());
+if n==0:
+    print("Zero");
+elif n<0:
+    print("Negative");
+else:
+    print("Positive")
