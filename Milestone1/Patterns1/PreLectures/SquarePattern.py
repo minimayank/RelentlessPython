79ee0d3edcb1800ff236d120cb78410dd62fcb9d
Mayank kumar <42250319+minimayank@users.noreply.github.com>
2022-09-18 23:35:01 +0530

    Square Pattern

---
+++ Milestone1/Patterns1/PreLectures/SquarePattern.py
@@ -0,0 +1,14 @@
+## Read input as specified in the question
+## Print the required output in given format
+
+n=int(input())
+
+i=0;
+
+while i<n:
+    j=0;
+    while j<n:
+        print(n,end="");
+        j+=1;
+    print();    
+    i+=1;    
