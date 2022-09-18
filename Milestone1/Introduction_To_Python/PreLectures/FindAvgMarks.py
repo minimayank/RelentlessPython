4942420fa83f33e5755179b782cd0ce8ccd661e9
Mayank kumar <42250319+minimayank@users.noreply.github.com>
2022-09-18 23:28:42 +0530

    FindAvgMarks

---
+++ Milestone1/Introduction_To_Python/PreLectures/FindAvgMarks.py
@@ -0,0 +1,10 @@
+# Read input as sepcified in the question
+# Print output as specified in the question
+
+def avg(a,b,c):
+    return (a+b+c)/3;
+
+a=int(input());
+b=int(input());
+c=int(input());
+print(avg(a,b,c));
