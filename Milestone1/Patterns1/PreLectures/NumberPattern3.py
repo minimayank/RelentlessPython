43bd3faf0cd11bfa3d7abb4e2b94d40142eb2ac6
Mayank kumar <42250319+minimayank@users.noreply.github.com>
2022-09-18 23:38:28 +0530

    Number Pattern 3

---
+++ Milestone1/Patterns1/PreLectures/NumberPattern3.py
@@ -0,0 +1,63 @@
+from os import *
+from sys import *
+from collections import *
+from math import *
+
+from os import *
+from sys import *
+from collections import *
+from math import *
+
+from os import *
+from sys import *
+from collections import *
+from math import *
+
+from os import *
+from sys import *
+from collections import *
+from math import *
+
+from os import *
+from sys import *
+from collections import *
+from math import *
+
+## Read input as specified in the question.
+## Print output as specified in the question.
+
+
+
+n=int(input());
+i=1;
+while i<=n:
+    j=1;
+    while j<=i:
+        if(j>1 and j<i):
+            print(2,end="");
+            
+        else:
+            print(1,end="");
+        j+=1;
+    print();    
+    i+=1;    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
