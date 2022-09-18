f7589b113cbaa9c4c81b145bdbfb34bab5481d48
Mayank kumar <42250319+minimayank@users.noreply.github.com>
2022-09-18 23:30:28 +0530

    Sum of n numbers.

---
+++ Milestone1/ConditionalsAndLoops/PreLectures/SumOfNNumber.py
@@ -0,0 +1,12 @@
+# Read input as sepcified in the question
+# Print output as specified in the question
+
+n=int(input());
+i=1;
+sum=0;
+while i<=n:
+    sum+=i;
+    i+=1;
+    
+    
+print(sum);
