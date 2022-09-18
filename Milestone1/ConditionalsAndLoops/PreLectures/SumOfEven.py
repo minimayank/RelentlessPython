66caf2aca8302ee60babef9c8acaf0690fe6c31b
Mayank kumar <42250319+minimayank@users.noreply.github.com>
2022-09-18 23:31:08 +0530

    Sum of Even numbers

---
+++ Milestone1/ConditionalsAndLoops/PreLectures/SumOfEven.py
@@ -0,0 +1,20 @@
+'''
+	Pre-requisite to solve this problem:
+	In this question, we have to find sum of all even numbers till n. To take sum of all the even numbers, we have to take a variable and initialize it to zero. Let us name this variable as sum. 
+	Following code is used to add 2 to sum variable and update it to the same sum variable:
+	sum = sum + 2 
+	
+	Hint to solve this problem: We will have to loop on even numbers only and add the even numbers to the same sum variable, described above.
+'''
+
+## Read input as specified in the question.
+## Print output as specified in the question.
+i=1;
+n=int(input());
+evenSum=0;
+while i<=n:
+    if i%2==0:
+        evenSum+=i;
+    i+=1;      
+
+print(evenSum);
